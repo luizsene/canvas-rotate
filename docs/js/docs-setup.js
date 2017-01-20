b4f91e8cdd2af8364a2de14e6e21a0b9ca951470
1fabiopereira <fabio.pereira.gti@hotmail.com>
2017-01-20 12:48:14 -0200

    Add ngdocs

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,48 @@
+NG_DOCS={
+  "sections": {
+    "api": "Canvas Rotate Docs"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "br.com.canvasRotate:canvasRotate",
+      "shortName": "canvasRotate",
+      "type": "directive",
+      "moduleName": "br.com",
+      "shortDescription": "Diretiva que fornece a possibilidade de rotacionar imagens para edição",
+      "keywords": "$ionicgesture 1800 242 2880 412 569 660 79 8100 90 abscissas altura angle angulo ao ap api aplica arco atual auxilia br caixa calcangular canvas canvasrotate carregada central class-name config configura contador contendo context contexto contido coordenadas coords counter da das de dentro desenha detectada deve directiva directive direita direito diretiva disparada drawrotatedimage edi efetua eixo elem element-id elemento elementos em escrita esquerdo est exibi fornece fun gerir gti html http image imagem imagens imagewrapper inferior inicial inicializa init initvars inst jpg lado largura manipula manipulada method obj objeto onde options ordenadas os overview par para parte pega pereira ponto por posi positionx positiony possibilidade presentes property quando radiano radians rela respons retorna rigth rota rotacionar rotate se sele seletor ser sera servi superior tamanho todas total true um uma url wrapper x0 x1 x2 y0 y1 y2"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "Canvas Rotate Docs",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
